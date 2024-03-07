f7413f633473d47727aa0b77b50ea575923aface
Santiago Urrego <santiagourregolaya@gmail.com>
2024-03-07 09:42:46 -0500

    primer commit

---
+++ src/es6/00-let-const.js
@@ -0,0 +1,22 @@
+var lastName = 'David';
+lastName = 'Oscar';
+console.log(lastName);
+
+let fruit = 'Apple';
+fruit = 'kiwi';
+console.log(fruit);
+
+const animal = 'Dog';
+animal = 'Cat';
+console.log(animal);
+
+const fruits = () => {
+  if (true) {
+    var fruit1 = 'Apple';
+    let fruit2 = 'kiwi';
+    const fruit3 = 'Banana';
+  }
+  console.log(fruit1);
+  console.log(fruit2);
+  console.log(fruit3);
+}
